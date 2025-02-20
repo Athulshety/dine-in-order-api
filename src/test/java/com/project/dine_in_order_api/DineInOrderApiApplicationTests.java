3aa0d1026bf43e1544a57eb5a8c83479320b0487
Athulshety <athulshetty907@gmail.com>
2025-02-20 09:06:56 +0530

    Project initiated

---
+++ src/test/java/com/project/dine_in_order_api/DineInOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dine_in_order_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DineInOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
